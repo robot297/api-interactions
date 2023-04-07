c82e8a398098777c06a13b64f3a6fd7cd2597410
Daniel Obot <danobot@hotmail.com>
2023-04-06 19:32:10 -0500

    feat: Added example money movement api using custom library

---
+++ python/bankapi_library.py
@@ -0,0 +1,28 @@
+"""This utilizes a python library that should help with using our APIs.
+"""
+import os
+from dotenv import load_dotenv
+from pprint import pprint
+
+# This import was something I created, you can install this via `pip install mctc_hackathon_robot297`
+from hackathon_helper.api.money_movement import MoneyMovement
+
+if __name__ == '__main__':
+    load_dotenv()  # if you have a .env file in your project, it'll load values in memory
+
+    money_movement = MoneyMovement(
+        api_key=os.getenv('api_key'),  # after loading your env variables, you can os.getenv the values
+        api_secret=os.getenv('api_secret')  # to know the values, look in the `.env` file or `.env-template` file
+    )
+
+    payload = { # sample payload to send to the library
+        "accountID": "00000",
+        "amount": 10,
+        "checkNumber": "",
+        "party": "Test"
+    }
+
+    response = money_movement.deposit_money(payload) # this method will call api and will return the entire response
+
+    print(response.status_code)
+    pprint(response.json())
